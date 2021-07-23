c484e43febbedee725170e751adae265a447b3a6
Lynda Chiwetelu <lyndachiwe@gmail.com>
2021-07-23 10:20:07 +0200

    is logged in

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,10 +1,13 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import UserController from '../controllers/UserController'
 import { authenticateToken } from '../middleware/Authenticate'
 import { validateLogin, validateSignup, validateUpdateUserSettings } from '../middleware/UserRequest'
 const router = express.Router()
 
 router.get('/', authenticateToken, UserController.getUsers)
+router.get('/isLoggedIn', authenticateToken, (req:Request, res:Response) => {
+    res.sendStatus(200)
+})
 router.post('/', validateSignup, UserController.createUser)
 router.post('/login', validateLogin, UserController.loginUser)
 router.patch('/:id/settings', authenticateToken, validateUpdateUserSettings, UserController.updateUserSettings)
